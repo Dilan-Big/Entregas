5282c5d2572e21b61e51dd05e19a48b6982c6241
Dilan Gonzalez <dilanbigdata@gmail.com>
2026-04-27 21:20:46 -0500

    try_catch

--- 27-04-2026/01-facke-api.js
+++ 27-04-2026/01-facke-api.js
@@ -1,10 +1,15 @@
 const API = 'https://fakeapi.net/products';
 
 async function getProducts(){
-    const response = await fetch (API);
-    const data = await response.json();
+    try {
+        const response = await fetch (API);
+        const data = await response.json();
 
     console.log(data);
+    } catch (error) {
+        console.log("error al conectarse al FackeAPI");
+    }
+    
 }
 
 getProducts();
