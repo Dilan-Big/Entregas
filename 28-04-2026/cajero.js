f39762b1f9eb1e59ea29f190407167c26cd3337c
Dilan Gonzalez <dilanbigdata@gmail.com>
2026-04-29 18:37:46 -0500

    Ingresar_valor

---
+++ 28-04-2026/cajero.js
@@ -0,0 +1,17 @@
+// Simular un cajero automatico:
+
+// 1. consignar dinero: recordar que no se puede consignar valores negativos
+
+let valorConsignar = Number(prompt("Ingrese el valor a consignar: "));
+
+const valorPositivo = (valorConsignar) => {
+    if (valorConsignar <= 0) {
+        return "¡El valor debe ser positivo!";
+    } else {
+        return "¡Se te dara ingreso al cajero!";
+    }
+}
+let resultado = valorPositivo(valorConsignar);
+
+console.log("El valor ingresado fue: " + valorConsignar);
+console.log(resultado);
